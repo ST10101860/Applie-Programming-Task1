63dd6947bc67153ba1769350e8235175e696144f
Latitude 7480 <Latitude 7480@THE-SMITH>
2023-10-21 12:27:45 +0200

    Add project files.

---
+++ AppliedProgrammingTask1/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppliedProgrammingTask1
+{
+    public class Connection
+    {
+        public string getConnection= @"Data Source=THE-SMITH\SQLEXPRESS;Initial Catalog=C2023_16;Integrated Security=True";
+    }
+}
