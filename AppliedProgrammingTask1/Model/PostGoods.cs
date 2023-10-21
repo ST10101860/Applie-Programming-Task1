b743edc1ad0b39c6f4d9f34063e8715ce17fb92d
Mashapole Raletsemo <Raletsm@telkom.co.za>
2023-10-21 14:52:00 +0200

    Models have being added to hold data

---
+++ AppliedProgrammingTask1/Model/PostGoods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedProgrammingTask1.Models
+{
+    public class PostGoods
+    {
+        public string date { get; set; }
+        public string totalItem { get; set; }
+        public string goodsSelection { get; set; }
+        public string otherGoods { get; set; }
+        public string goodDescription { get; set; }
+        public string status { get; set; }
+    }
+}
---
+++ AppliedProgrammingTask1/Model/PostMoney.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedProgrammingTask1.Models
+{
+    public class PostMoney
+    {
+        public string date { get; set; }
+        public double totalDonation { get; set; }
+        public string status { get; set; }
+    }
+}
