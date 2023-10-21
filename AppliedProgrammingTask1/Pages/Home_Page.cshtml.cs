d5271a968e9df81a999bdce237947157adb366d8
Mashapole Raletsemo <Raletsm@telkom.co.za>
2023-10-21 13:59:50 +0200

    admin Home page is being added to hold important links for admin

---
+++ AppliedProgrammingTask1/Pages/Home_Page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppliedProgrammingTask1.Pages
+{
+    public class Home_PageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
