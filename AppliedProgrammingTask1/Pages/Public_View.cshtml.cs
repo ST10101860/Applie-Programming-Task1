38a4d05fa3cc9d16468e55e3db9e09e7018ad049
Mashapole Raletsemo <Raletsm@telkom.co.za>
2023-11-18 13:43:29 +0200

    Public page is being added

---
+++ AppliedProgrammingTask1/Pages/Public_View.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppliedProgrammingTask1.Pages
+{
+    public class Public_ViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
